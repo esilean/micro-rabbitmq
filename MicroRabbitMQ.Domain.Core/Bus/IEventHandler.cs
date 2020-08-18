cc91c854c412361108720aaf4c025e99a7dd22cf
esilean <le.bevilaqua@gmail.com>
2020-08-18 13:46:24 -0300

    implemented interfaces and classes for domain.core

---
+++ MicroRabbitMQ.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using MicroRabbitMQ.Domain.Core.Events;
+
+namespace MicroRabbitMQ.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler { }
+}
---
+++ MicroRabbitMQ.Domain.Core/Commands/Command.cs
@@ -0,0 +1,14 @@
+using System;
+using MicroRabbitMQ.Domain.Core.Events;
+
+namespace MicroRabbitMQ.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; protected set; }
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
